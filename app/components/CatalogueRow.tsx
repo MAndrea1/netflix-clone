83300e78cc07d40ba65926fb1d78f6d83576f5a9
Micaela <m.andrea.higa@gmail.com>
2023-10-10 14:52:44 -0300

    add transition to row arrows

--- app/components/CatalogueRow.tsx
+++ app/components/CatalogueRow.tsx
@@ -28,12 +28,16 @@ const CatalogueRow = ({title, movieList}: propsType) => {
   return (
     <div className='mt-4 md:mt-6'>
       <h2 className='font-bold text-xs md:text-sm md:mb-2 lg:text-2xl'>{title}</h2>
-      <div className='group relative'>
-        <span className={`hidden drop-shadow-md absolute cursor-pointer text-4xl pl-3 z-50 md:pt-10 lg:text-5xl lg:pl-4 lg:pt-12 ${moved === 1 && 'md:flex'}`} onClick={() => handleClick(1)}>‹</span>
+      <div className='group relative hover:transition-all'>
+        <div className={`hidden absolute z-50 items-center cursor-pointer transition duration-500 hover:bg-stone-950 hover:bg-opacity-50 ${moved === 1 && 'md:flex'} md:h-44 md:pb-2 md:items-center lg:h-36`} onClick={() => handleClick(1)}>
+          <span className={`clickable drop-shadow-md text-4xl px-3 lg:text-5xl lg:px-4`}>‹</span>
+        </div>
         <div ref={reference} className='flex items-center overflow-x-scroll space-x-1 no-scrollbar md:space-x-2'>
           {movieList?.map(movie => <Thumbnail key={movie.id} movie={movie}/>)}
         </div>
-        <span className='hidden drop-shadow-md absolute right-0 top-0 cursor-pointer text-4xl pr-1 z-50 md:flex md:pt-10 lg:text-5xl lg:pt-12'onClick={() => handleClick(-1)}>›</span>
+        <div className={`hidden absolute right-0 top-0 z-50 items-center cursor-pointer transition duration-500 hover:bg-stone-950 hover:bg-opacity-50 md:flex md:h-44 md:pb-2 md:items-center lg:h-36`} onClick={() => handleClick(-1)}>
+          <span className={`clickable drop-shadow-md text-4xl px-3 lg:text-5xl lg:px-4`}>›</span>
+        </div>
       </div>
     </div>
   )
