a51cede1778526c231dd0a73adb273b581f8036a
Micaela <m.andrea.higa@gmail.com>
2023-10-14 11:17:31 -0300

    add firebase config file and modify app folder structure

---
+++ app/(login)/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <main className="relative flex min-h-screen flex-col">
+        <h1>Page</h1>
+      </main>
+    </>
+  )
+}
+
+export default page
