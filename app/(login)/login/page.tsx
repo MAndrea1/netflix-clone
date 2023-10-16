317fb2ac713a01b843653fa8c6c238644e87d9d4
Micaela <m.andrea.higa@gmail.com>
2023-10-16 14:03:17 -0300

    toggle sign in/up

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -11,7 +11,7 @@ type Inputs = {
 };
 
 const Login = () => {
-  const [askLogin, setAskLogin] = useState(false)
+  const [ askLogin, setAskLogin] = useState(false)
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
   const { signUp, login } = useAuth()
 
@@ -42,7 +42,7 @@ const Login = () => {
         <div className="mt-20 px-5 w-full md:w-[30rem] md:bg-black md:bg-opacity-80 md:p-20 md:mt-0">
           <form className="pb-5 md:pb-20" onSubmit={handleSubmit(onSubmit)}>
             <fieldset>
-              <legend className="text-3xl font-semibold">Sign In</legend>
+              <legend className="text-3xl font-semibold">{askLogin ? "Sign In" : "Sign up"}</legend>
               <div className="flex flex-col my-8 space-y-4">
                 <label>
                   <input type="email" placeholder="Email" className={`input w-full ${errors.emailField && "border-b-2 border-orange-500"}`} defaultValue={""}  {...register("emailField", { required: true })} />
@@ -53,12 +53,12 @@ const Login = () => {
                   {errors.passwordField && <div className="text-xs text-orange-500 mt-2">Your password must contain between 4 and 60 characters.</div>}
                 </label>
               </div>
-              <button type="submit" className="nred py-4 px-3 font-semibold rounded md w-full">Sign In</button>
+              <button type="submit" className="nred py-4 px-3 font-semibold rounded md w-full">{askLogin ? "Sign in" : "Sign up"}</button>
             </fieldset>
           </form>
         <div className="text-sm text-stone-400">
-          <span>New to Netflix? </span>
-          <button className="text-white hover:underline" onClick={() => setAskLogin(true)}>Sign up now.</button>
+          <span>{askLogin ? "New to Netflix? " : "Already have an account? "}</span>
+          <button className="text-white hover:underline" onClick={() => setAskLogin(!askLogin)}>{askLogin ? "Sign up now" : "Log in"}</button>
         </div>
         </div>
         <Image 
