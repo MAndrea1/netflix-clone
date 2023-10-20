485adf4f39ec76599ffc1a87eb48a8b5866a7fbc
Micaela <m.andrea.higa@gmail.com>
2023-10-20 13:57:03 -0300

    fix close button not showing in mobile

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -68,12 +68,12 @@ const Modal = () => {
         aria-describedby="modal-modal-description"
         className={`${movieState? 'overflow-hidden' : 'overflow-scroll'}`}
         >
-        <div className={`absolute w-[100vw] h-[100vh] transform ${movieState? 'bottom-0 right-0': '-translate-x-1/2 md:min-w-[60vw] md:max-w-full lg:w-[60vw] md:h-fit left-1/2 lg:top-1/2 lg:-translate-y-1/2'}`}>
-          <button onClick={handleClose} className={`absolute transition duration-[.4s] right-4 top-4 z-50 h-12 w-12 lg:h-10 lg:w-10 rounded-full bg-slate-950 lg:bg-transparent  ${movieState? 'opacity-50 lg:opacity-0 lg:hover:opacity-100' : 'hover:bg-stone-800 hover:bg-opacity-50'}`}>
+        <div className={`absolute w-[100vw] h-[100vh] transform ${movieState? '': '-translate-x-1/2 md:min-w-[60vw] md:max-w-full lg:w-[60vw] md:h-fit left-1/2 lg:top-1/2 lg:-translate-y-1/2'}`}>
+          <button onClick={handleClose} className={`absolute transition duration-[.4s] right-4 top-4 z-50 h-12 w-12 lg:h-10 lg:w-10 rounded-full bg-slate-950 lg:bg-transparent  ${movieState? 'opacity-50 lg:opacity-0 hover:opacity-100' : 'hover:bg-stone-800 hover:bg-opacity-50'}`}>
             <span className='h-6 w-6 select-none'>X</span>
           </button>
-          <div className={`flex flex-col bg-stone-950 ${movieState? '' : 'max-h-[70vh]'}`}>
-            <div className={`aspect-video ${movieState? 'w-[100vw] h-[100vh]' : 'lg:w-[60vw]'}`}>
+          <div className={`flex z-50 justify-center flex-col bg-stone-950 ${movieState? 'w-[100dvw] h-[100dvh]' : 'max-h-[70vh]'}`}>
+            <div className={`aspect-video ${movieState? '' : 'lg:w-[60vw]'}`}>
               <ReactPlayer 
               url={`https://www.youtube.com/watch?v=${trailer}`}
               width="100%"
